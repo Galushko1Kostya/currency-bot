1e2fc523ae866dabfd40ab2ba7aa151f41a230c7
Galushko1Kostya <ks.galushko@gmail.com>
2022-03-19 11:05:03 +0200

    json2

--- bot.py
+++ bot.py
@@ -12,53 +12,41 @@ def welcome(update, context):
     buttons = [[KeyboardButton('USD')], [KeyboardButton('EUR')], [KeyboardButton('PLN')], [KeyboardButton('GEL')], [KeyboardButton('CAD')], [KeyboardButton('MXN')], [KeyboardButton('MDL')], [KeyboardButton('ILS')], [KeyboardButton('NOK')], [KeyboardButton('IDR')]]
     context.bot.send_message(chat_id=chat.id, text='Hello! I am your currency bot',
                              reply_markup=ReplyKeyboardMarkup(buttons))
-
-def create_message_to_json(currency_code, date, rate):
-    global data
-    data = {"currency code" : currency_code, "date" : date, "rate" : rate}
-    
-def write_to_file():
-    with open("new.json", "a") as f:
-        f.append(json.dumps(data))
-
-def dates():
-    global date_22
-    global date_21
-    date_22 = datetime.datetime.now()
-    date_21 = str(datetime.datetime.now() - datetime.timedelta(days=365))
+def logging(a, b):
+    neww.json
+    [{
+        'login':'',
+        'password':'',
+    }]
+    pass
 
 def currency_rate_22(update, context):
     chat = update.effective_chat
     currency_code = update.message.text
+    global date_22
+    global date_21
+    date_22 = datetime.datetime.now()
+    date_21 = str(datetime.datetime.now() - datetime.timedelta(days=365))
     if currency_code in ('USD', 'EUR', 'PLN', 'GEL', 'CAD', 'MXN', 'MDL', 'ILS', 'NOK', 'IDR'):
         currency_rate1 = requests.get(f'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode='
                                       f'{currency_code}&date='f'{date_21}&json').json()
-        rate = currency_rate1[0]['rate']
-        global value_new
-        value_new = f'{currency_code} rate: {rate} UAH'
-    context.bot.send_message(chat_id=chat.id, text=value_new)
-
-def currency_rate_21(update, context):
-    chat = update.effective_chat
-    currency_code = update.message.text
-    if currency_code in ('USD', 'EUR', 'PLN', 'GEL', 'CAD', 'MXN', 'MDL', 'ILS', 'NOK', 'IDR'):
         currency_rate2 = requests.get(f'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode='
-                                      f'{currency_code}&date='f'{date_22}&json').json()
-        rate = currency_rate2[0]['rate']
-        global value_old
-        value_old = f'{currency_code} rate: {rate} UAH'
-    context.bot.send_message(chat_id=chat.id, text=value_old)
-
-def sum(update, context):
-    chat = update.effective_chat
+                                      f'{currency_code}&date='f'{date_22}&json').json()                              
+        rate1 = currency_rate1[0]['rate']
+        rate2 = currency_rate2[0]['rate']
+        value_new = f'{currency_code} rate: {rate1} UAH'
+        value_old = f'{currency_code} rate: {rate2} UAH'
     message_sum = value_new - value_old
     context.bot.send_message(chat_id=chat.id, text=message_sum)
-    write_to_file(create_message_to_json())
+
+def create_message_to_json(currency_code, rate, message_sum):
+    data = {"currency code" : currency_code, "rate" : rate, "result" : message_sum}
+    with open("neww.json", "a") as f:
+        f.append(json.dumps(data))
 
 disp = updater.dispatcher
 disp.add_handler(CommandHandler('start', welcome))
-disp.add_handler(CommandHandler('currency_rate_new', currency_rate_22))
-disp.add_handler(CommandHandler('currency_rate_old', currency_rate_21))
+disp.add_handler(CommandHandler('json', create_message_to_json))
 disp.add_handler(MessageHandler(Filters.all, sum))
 
 updater.start_polling()
